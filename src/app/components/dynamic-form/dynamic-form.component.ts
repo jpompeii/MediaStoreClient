37e01356a20cafcd66cccc83f2580d96a355db5f
Chaz Gatian <CharlesGatian@hotmail.com>
2018-02-14 22:00:24 -0500

    feat(dynamic-form): add sections and basic sorting

--- src/app/components/dynamic-form/dynamic-form.component.ts
+++ src/app/components/dynamic-form/dynamic-form.component.ts
@@ -1,11 +1,19 @@
 import { Component, OnInit } from '@angular/core';
-import { FormBuilder, FormGroup } from '@angular/forms';
+import { FormBuilder, FormGroup, Validators, ValidatorFn } from '@angular/forms';
 
 
 export interface Field {
   type: string;
+  required?: boolean;
   name: string;
   label: string;
+}
+
+export interface Section  {
+  id: string;
+  title?: string;
+  standalone?: boolean;
+  fields: (TextField | DateField)[];
 }
 
 export interface TextField extends Field {
@@ -17,6 +25,12 @@ export interface DateField extends Field {
   value: Date;
 }
 
+export interface DynamicFormConfig {
+  title: string;
+  sections: Section[];
+  sectionOrder: string[];
+}
+
 @Component({
   selector: 'app-dynamic-forms',
   templateUrl: './dynamic-form.component.html',
@@ -26,26 +40,62 @@ export class FormBuilderComponent implements OnInit {
 
   _form: FormGroup;
 
-  _formConfiguration: (TextField | DateField)[] = [
-    {
-      name: 'address1',
-      type: 'text',
-      label: 'Address #1',
-      value: '594 Bell Ave'
-    },
-    {
-      name: 'address2',
-      type: 'text',
-      label: 'Address #2',
-      value: 'Apt 122'
-    },
-    {
-      name: 'purchaseDate',
-      type: 'date',
-      label: 'Purchase Date',
-      value: new Date('1/11/1985')
-    }
-  ];
+  _formConfiguration: DynamicFormConfig = {
+    title: 'Test Form',
+    sectionOrder: ['last', 'first', 'second' ],
+    sections: [
+      {
+        id: 'first',
+        standalone: true,
+        fields: [
+          {
+            name: 'invoice',
+            type: 'text',
+            required: true,
+            label: 'Invoice Number',
+            value: '1234'
+          },
+        ]
+      },
+      {
+        id: 'second',
+        title: 'ONE',
+        fields: [
+          {
+            name: 'address1',
+            type: 'text',
+            label: 'Address #1',
+            value: '594 Bell Ave'
+          },
+          {
+            name: 'address2',
+            type: 'text',
+            label: 'Address #2',
+            value: 'Apt 122'
+          },
+          {
+            name: 'purchaseDate',
+            type: 'date',
+            label: 'Purchase Date',
+            value: new Date('1/11/1985')
+          }
+        ]
+      },
+      {
+        id: 'last',
+        title: 'License Info',
+        fields: [
+          {
+            name: 'drivers_license',
+            type: 'text',
+            required: true,
+            label: `Driver's License`,
+            value: 'SD03982'
+          },
+        ]
+      }
+    ]
+  };
 
   constructor(private fb: FormBuilder) { }
 
@@ -54,8 +104,35 @@ export class FormBuilderComponent implements OnInit {
   }
 
   private buildForm() {
+
+    const form = this.fb.group({
+    });
+
+    this._formConfiguration.sections.forEach(section => {
+      form.addControl(section.id, this.convertFields(section.fields));
+    });
+    return form;
+  }
+
+  private convertFields(fields: (TextField | DateField)[]): FormGroup {
     const group = this.fb.group({});
-    this._formConfiguration.forEach(control => group.addControl(control.name, this.fb.control(control.value)));
+    fields.forEach(field => {
+      group.addControl(field.name, this.fb.control(field.value, this.generateValidators(field)));
+    });
     return group;
+  }
+
+  _sortSections(config: DynamicFormConfig): Section[] {
+    return config.sectionOrder.map(sectionId => {
+      return config.sections.find(section => section.id === sectionId);
+    });
+  }
+
+  private generateValidators(field: Field): ValidatorFn[] {
+    const validators = [];
+    if (field.required) {
+      validators.push(Validators.required);
+    }
+    return validators;
   }
 }
